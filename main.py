2157afb5ee04dc6643ed587f36ebfec02d9dfc07
Manuel Alejandro <malebps@gmail.com>
2018-03-04 21:49:48 -0500

    Phase 3.0
    Fully functional prototype

--- main.py
+++ main.py
@@ -6,8 +6,9 @@
 import datetime
 import time
 
-## Global vars definition
+## Global vars
 looper = True
+looperval = 0
 
 ## Global functions
         
@@ -30,23 +31,23 @@ class trafficRestriction:
         
     def trafficRestrictionDay(self):
         if self.digit in range(1,3) and self.dow == 0:
-            print "Match 1 or 2"
+            #print "Match 1 or 2"
             return 1
         elif self.digit in range(3,5) and self.dow == 1:
-            print "Match 3 or 4"
+            #print "Match 3 or 4"
             return 1
         elif self.digit in range(5,7) and self.dow == 3:
-            print "Match 5 or 6"
+            #print "Match 5 or 6"
             return 1
         elif self.digit in range(7,9) and self.dow == 4:
-            print "Match 7 or 8"
+            #print "Match 7 or 8"
             return 1
         elif self.digit == 9 and self.dow == 5:
             return 1
         elif self.digit == 0 and self.dow == 5:
             return 1
         else:
-            print "Did not match"
+            #print "Did not match"
             return 0
 
     def trafficRestrictionTime(self):
@@ -58,19 +59,57 @@ class trafficRestriction:
 ## Main program
 while looper == True:
     print "TRAFFIC RESTRICTION PREDICTOR \nThis application will let you know if your car is allowed for circulation based on the License Plate and an specific date you are planning to drive \nLet's get started \n"
-    licensePlateId = str(raw_input("Enter the license plate. I.e. ABC1234 : "))
-    dateToDrive = str(raw_input("Enter the date you plan to drive. I.e. DD/MM/YYYY : "))
-    timeToDrive = str(raw_input("Enter the time that you will start to drive. I.e. HH:MM : "))
-
-    ## Parser 
+    
+    ## Input checker
+    while True:
+        licensePlateId = str(raw_input("Enter the license plate. I.e. ABC1234 : "))
+        if len(licensePlateId) == 7:
+            if licensePlateId[3:].isdigit() and not licensePlateId[0].isdigit() and not licensePlateId[1].isdigit() and not licensePlateId[2].isdigit(): # Not validated for special characters, though. Next phase will support this feature.
+                break
+            else:
+                print "Wrong entry, the format used should be AAA1234"
+        else:
+            print "Wrong entry, the format used should be AAA1234"
+   
+    while True:
+        dateToDrive = str(raw_input("Enter the date you plan to drive. I.e. DD/MM/YYYY : "))
+        if len(dateToDrive) == 10:
+            #print "Checking time: ",dateToDrive[0:2],dateToDrive[3:5],dateToDrive[6:10]
+            if dateToDrive[0:2].isdigit() and dateToDrive[3:5].isdigit() and dateToDrive[6:10].isdigit():
+                if 0 < int(dateToDrive[0:2]) <= 31 and 0 < int(dateToDrive[3:5]) <= 12 and 2000 < int(dateToDrive[6:10]) < 2050:
+                    break
+                else:
+                    print "Wrong entry, the format used should be DD/MM/YYYY. Date must be between 1999 to 2050"
+            else:
+                print "Wrong entry, the format used should be DD/MM/YYYY. Date must be between 1999 to 2050"
+        else:
+            print "Wrong entry, the format used should be DD/MM/YYYY. Date must be between 1999 to 2050"
+            
+    while True:
+        timeToDrive = str(raw_input("Enter the time that you will start to drive. I.e. HH:MM : "))
+        if len(timeToDrive) == 5:
+            #print "Checking time: ",timeToDrive[0:2],timeToDrive[3:5]
+            if timeToDrive[0:2].isdigit() and timeToDrive[3:5].isdigit():
+                if 0 < int(timeToDrive[0:2]) <= 23 and 0 <= int(timeToDrive[3:5]) <= 59:
+                    break
+                else:
+                    print "Wrong entry, the format used should be HH:MM."
+            else:
+                print "Wrong entry, the format used should be HH:MM."
+        else:
+            print "Wrong entry, the format used should be HH:MM."
+            
+    ## Populate variables
     driveDateTime = datetime.datetime(int(dateToDrive[6:10]),int(dateToDrive[3:5]),int(dateToDrive[0:2]),int(timeToDrive[0:2]),int(timeToDrive[-2:]))
     
-    ## End Parser
-    
     print "Processing, please wait...\n"
-    time.sleep(1)
+    time.sleep(2)
+    
+    ## Objects creation
     customerLP = licensePlate(licensePlateId)
     restrictionCheck = trafficRestriction(customerLP.findLastDigit(), driveDateTime)
+    
+    ## Core program, traffic restriction validation
     isRestrictedDay = restrictionCheck.trafficRestrictionDay()
     if isRestrictedDay == 1:
         isRestrictedTime = restrictionCheck.trafficRestrictionTime()
@@ -81,4 +120,15 @@ while looper == True:
     else:
         print "OK: You can drive :)"
     print "\n\n\n"
-    time.sleep(3)
+    
+    print "Further options\nIf you wish to continue checking, enter yes or any key.\n"
+    looperval = raw_input("Do you wish to continue? [yes/no] ")
+    if len(looperval) != 0:
+        if str(looperval) == "no" or str(looperval) == "n" or str(looperval) == "No" or str(looperval) == "NO" or str(looperval) == "N" or str(looperval) == "nO":
+            print "\n\n\nGoodbye!\n\n"
+            break
+    else:
+        looperval = "carryon"
+    time.sleep(1)
+    print "\n\n\n\n"
+    
